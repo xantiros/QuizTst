6829f880a9f45b7d2fd366e11094194950a548e9
Xantiros <xantiros@gmail.com>
2018-10-29 11:28:59 +0100

    Add Question.cs

---
+++ QuizTst.Core/Models/Question.cs
@@ -0,0 +1,28 @@
+﻿namespace QuizTst
+{
+    public class Question
+    {
+        public int Id { get; private set; }
+        public string Content { get; private set; }
+        public string A { get; private set; }
+        public string B { get; private set; }
+        public string C { get; private set; }
+        public string D { get; private set; }
+        public string Correct { get; private set; }
+
+        public Question(int id, string content, string a, string b, string c, string d, string correct)
+        {
+            Id = id;
+            Content = content;
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+            Correct = correct;
+        }
+
+        protected Question()
+        {
+        }
+    }
+}
--- QuizTst/Form1.Designer.cs
+++ QuizTst/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(134, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(393, 309);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
