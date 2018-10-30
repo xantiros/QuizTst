e67c1ad42d6dfe162eea391db9aa59d4409ec9f9
Xantiros <xantiros@gmail.com>
2018-10-30 12:04:33 +0100

    Player model

---
+++ QuizTst.Core/Models/Player.cs
@@ -0,0 +1,26 @@
+﻿namespace QuizTst.Core.Models
+{
+    public class Player
+    {
+        public int QuestionId { get; private set; }
+        public int Points { get; private set; }
+
+        public Player(int questionId, int points)
+        {
+            QuestionId = questionId;
+            Points = points;
+        }
+        public void AddPoint()
+        {
+            Points++;
+        }
+        public void NextQuestionId()
+        {
+            QuestionId++;
+        }
+
+        protected Player()
+        {
+        }
+    }
+}
