96fc24f21eb9df0f204ff1e9557ffc1535e21d30
Xantiros <xantiros@gmail.com>
2018-11-20 14:38:16 +0100

    Answered Questions

---
+++ QuizTst.Core/Models/AnsweredQuestions.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizTst.Core.Models
+{
+    public class AnsweredQuestions
+    {
+        public int Id { get; private set; }
+        public int Level { get; private set; }
+
+        public AnsweredQuestions(int id, int level)
+        {
+            Id = id;
+            Level = level;
+        }
+        protected AnsweredQuestions()
+        {
+        }
+    }
+}
